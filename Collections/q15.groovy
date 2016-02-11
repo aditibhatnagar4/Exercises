1f989f8d880072d8a38f8a143b10d7e3a04b3f43
aditi <aditi.bhatnagar@tothenew.com>
2016-02-11 09:25:29 +0530

    Unit Testing

--- Collections/q15.groovy
+++ Collections/q15.groovy
@@ -2,35 +2,40 @@ class Stack
 {
   List l=[];
   int top=-1
-  void push(String x)
+  void push(x)
   {
-   top++;
-   l.add(x)
-   println x+" pushed"
+//   top++;
+//   l.add(x)
+//   println x+" pushed"
+l.push(x)
+
   }
   
   def pop()
   {
-    if(top>=0)
-    {
-    int index=top
-    top--
-    return l.remove(index)
-    }
-    else
-    return "Stack is empty"
+//    if(top>=0)
+//    {
+//    int index=top
+//    top--
+//    return l.remove(index)
+//    }
+//    else
+//    return "Stack is empty"
+return l.pop()
+
   }
   
-  void top()
+  def top()
   {  
-     if(top>=0)
-     println l.last()
-     else
-     println "Empty Stack"
+//     if(top>=0)
+//     println l.last()
+//     else
+//     println "Empty Stack"
+return l.head()
   }
 }
 Stack obj=new Stack();
-obj.top();
+//obj.top();
 obj.push("Aditi")
 obj.push("Aarushi")
 obj.push("Aashna");
@@ -38,5 +43,5 @@ obj.top();
 println obj.pop()+" popped";
 obj.top();
 println obj.pop()+" popped";
-println obj.pop()+" popped";
+//println obj.pop()+" popped";
 println obj.pop()+" popped";
